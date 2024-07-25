b31ea9c5b8339ecd903aabe7712d792dea6b6e04
truclam <trandongtruclam@gmail.com>
2024-07-25 21:05:20 +0700

    chore: Update top categories grid layout to display 3 columns instead of 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange>
-          <main>{children}</main>
+          <main className="flex-1">{children}</main>
           <Footer/>
         </ThemeProvider>
       </body>
