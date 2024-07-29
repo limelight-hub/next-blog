05e5b4724bbd58a1c12b332ad94ee8296a93af8b
scarlet <trandongtruclam@gmail.com>
2024-07-29 16:57:54 +0700

    dkjb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Fouter from "@/components/footer";
+import Fouter from "@/components/fouter";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/components/fouter.tsx
+++ src/components/fouter.tsx
@@ -18,7 +18,7 @@ export default function Fouter() {
                     <div className="space-y-4">
                         <div className="flex items-center space-x-2">
                             <Icons.logo className="h-6 w-6" />
-                            <span className="text-md font-semibold">Scarlet's Blog</span>
+                            <span className="text-md font-semibold">Scarlet&apos;s Blog</span>
                         </div>
                         <p className="text-gray-500 dark:text-gray-400 text-sm">
                             Stay Up to Date with the latest news and insights from our blog.
