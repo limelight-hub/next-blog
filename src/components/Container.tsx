e12b92cb6ae9c28be914da85a9b85633c8604ab1
scarlet <trandongtruclam@gmail.com>
2024-07-29 14:47:50 +0700

    sfh

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import {cn} from '../lib/utils'
 
 export default function Container ({className, children} : {className?: string, children: React.ReactNode}) {
     return (
-        <div className={cn("container mx-auto px-20 py-4 ", className)}>
+        <div className={cn("container mx-auto w-2/3 py-1 ", className)}>
             {children}
         </div>
     )
